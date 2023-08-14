794f69bcacaacf81d1a2659477fccc5b814dadb6
root <eodkwokm@gmail.com>
2023-08-14 17:35:37 +0100

    stuff

---
+++ swap.js
@@ -0,0 +1,174 @@
+// Global
+const connect_button = document.getElementById("connect-wallet");
+
+// Swap page
+const swap_button = document.getElementById("swap-button");
+const input_button = document.getElementById("input-image");
+const output_button = document.getElementById("output-image");
+const switch_button = document.getElementById("switch-button");
+const slippage_button = document.getElementById("slippage-button");
+
+// Pools page
+const add_liquidity_buttons = document.querySelectorAll(".add-liquidity");
+const remove_liquidity_buttons = document.querySelectorAll(".remove-liquidity");
+const claim_rewards_buttons = document.querySelectorAll(".claim-rewards");
+
+
+swap_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    Toastify({
+        text: "Not implemented!",
+        duration: 3000,
+        avatar: "x-icon.svg",
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FF2400",
+          color: "#fff",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+});
+
+input_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    Toastify({
+        text: "Not implemented!",
+        duration: 3000,
+        avatar: "x-icon.svg",
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FF2400",
+          color: "#fff",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+});
+
+output_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    Toastify({
+        text: "Not implemented!",
+        duration: 3000,
+        avatar: "x-icon.svg",
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FF2400",
+          color: "#fff",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+});
+
+switch_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    Toastify({
+        text: "Not implemented!",
+        duration: 3000,
+        avatar: "x-icon.svg",
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FF2400",
+          color: "#fff",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+});
+
+slippage_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    Toastify({
+        text: "Not implemented!",
+        duration: 3000,
+        avatar: "x-icon.svg",
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FF2400",
+          color: "#fff",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+});
+
+add_liquidity_buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        Toastify({
+            text: "Not implemented!",
+            duration: 3000,
+            avatar: "x-icon.svg",
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: false, // Prevents dismissing of toast on hover
+            style: {
+              background: "#FF2400",
+              color: "#fff",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+    });
+});
+
+remove_liquidity_buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        Toastify({
+            text: "Not implemented!",
+            duration: 3000,
+            avatar: "x-icon.svg",
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: false, // Prevents dismissing of toast on hover
+            style: {
+              background: "#FF2400",
+              color: "#fff",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+    });
+});
+
+claim_rewards_buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        Toastify({
+            text: "Not implemented!",
+            duration: 3000,
+            avatar: "x-icon.svg",
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: false, // Prevents dismissing of toast on hover
+            style: {
+              background: "#FF2400",
+              color: "#fff",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+    });
+});
+
+connect_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    Toastify({
+        text: "Not implemented!",
+        duration: 3000,
+        avatar: "x-icon.svg",
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background: "#FF2400",
+          color: "#fff",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+});
